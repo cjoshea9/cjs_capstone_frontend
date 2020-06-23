48f0104cf5f67304a31ff2ef5d4dcf01707adf40
Cory O'Shea <cjoshea9@gmail.com>
2020-06-23 14:39:07 -0600

    Add navbar

---
+++ src/components/Navbar.js
@@ -0,0 +1,18 @@
+import React from 'react';
+import AppBar from '@material-ui/core/AppBar'
+import ToolBar from '@material-ui/core/Toolbar'
+import Typography from '@material-ui/core/Typography'
+
+const Navbar = () => {
+    return (
+        <AppBar position="static">
+            <ToolBar>
+                <Typography variant="title" color="inherit">
+                    CodeTranslate
+                </Typography>
+            </ToolBar>
+        </AppBar>
+    );
+}
+
+export default Navbar;
