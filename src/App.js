8a9e5877a2e0ffce760dc60a4233fa9ced49fe4b
Cory O'Shea <cjoshea9@gmail.com>
2020-06-24 12:25:38 -0600

    Allows ouput textbox to take data from the backend

--- src/App.js
+++ src/App.js
@@ -2,35 +2,54 @@ import React from 'react';
 import './App.css';
 import Navbar from "./components/Navbar"
 import TextField from '@material-ui/core/TextField';
+import Button from '@material-ui/core/Button';
+import { render } from '@testing-library/react';
 
-function App() {
-  async function getRequest() {
-    const res = await fetch('https://cjsback.herokuapp.com/')
-    const data = await res.json()
-    console.log(data)
+class App extends React.Component {
+  constructor(props){
+    super(props);
+    this.state = {
+      value: ""
+    }
+    this.getRequest = this.getRequest.bind(this)
   }
   
-  getRequest()
-  return (
-    <div>
-      <Navbar/>
-      <div className="textfields">
-        <TextField
-          className="filled-textarea"
-          label="Enter Text"
-          multiline
-          variant="filled"
-        />
-        <TextField
-          className="filled-textarea"
-          label="Translate"
-          multiline
-          variant="filled"
-        />
-      </div>
-    </div>
+  getRequest() {
+    fetch('https://cjsback.herokuapp.com/')
+      .then(res => res.json())
+      .then((data) => {
+        console.log(data)
+        this.setState({
+          value: data['hello']
+        })
+      })
+  }
 
-  );
+  render() {
+    return (
+      <div>
+        <Navbar/>
+        <div className="textfields">
+          <TextField
+            className="filled-textarea"
+            label="Enter Text"
+            multiline
+            variant="filled"
+          />
+          <TextField
+            className="filled-textarea"
+            label="Translate"
+            multiline
+            variant="filled"
+            value={this.state.value}
+          />
+          <Button variant="contained" color="primary" onClick={this.getRequest}>
+            Translate
+          </Button>
+        </div>
+      </div>
+    );
+  }
 }
 
 export default App;
