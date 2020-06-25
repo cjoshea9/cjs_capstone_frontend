bfaa4a0af7659df3bb0d866f2b5c229ff65609bc
Stephanie Walsh <srwalsh21@gmail.com>
2020-06-25 12:25:56 -0400

    Post request send input and output lang

--- src/App.js
+++ src/App.js
@@ -3,13 +3,19 @@ import './App.css';
 import Navbar from "./components/Navbar"
 import TextField from '@material-ui/core/TextField';
 import Button from '@material-ui/core/Button';
+import Select from '@material-ui/core/Select';
+import InputLabel from '@material-ui/core/InputLabel';
+import MenuItem from '@material-ui/core/MenuItem';
+import FormControl from '@material-ui/core/FormControl';
 
 class App extends React.Component {
   constructor(props){
     super(props);
     this.state = {
       input: "",
-      output: ""
+      output: "",
+      inputLanguage: "",
+      outputLangauge: ""
     }
     this.postRequest = this.postRequest.bind(this)
     this.updateInput = this.updateInput.bind(this)
@@ -21,7 +27,7 @@ class App extends React.Component {
       headers: new Headers({
         'Content-Type': 'application/x-www-form-urlencoded',
       }),
-      body: "input="+ this.state.input
+      body: "input="+ this.state.input + "&in_lang=" + this.state.inputLanguage + "&out_lang" + this.state.outputLangauge
     })
       .then(res => res.json())
       .then((data) => {
@@ -37,10 +43,55 @@ class App extends React.Component {
     })
   }
 
+  updateInputLanguage = (input) => {
+    this.setState({
+        inputLanguage: input.target.value
+    });
+  }
+
+  updateOutputLanguage = (input) => {
+    this.setState({
+        outputLangauge: input.target.value
+    });
+  }
+
   render() {
     return (
       <div>
         <Navbar/>
+        
+       <div class="selectlanguage">
+       <FormControl>
+        <InputLabel id="demo-controlled-open-select-label">Input</InputLabel>
+       <Select
+          value={this.inputLanguage}
+          onChange={this.updateInputLanguage}
+        >
+          <MenuItem value="">
+            <em>None</em>
+          </MenuItem>
+          <MenuItem value={"js"}>JavaScript</MenuItem>
+          <MenuItem value={"py"}>Python</MenuItem>
+        </Select>
+        </FormControl>
+        
+       
+        <FormControl>
+        <InputLabel id="demo-controlled-open-select-label">Output</InputLabel>
+        <Select
+          value={this.outputLangauge}
+          onChange={this.updateOutputLanguage}
+        >
+          <MenuItem value="">
+            <em>None</em>
+          </MenuItem>
+          <MenuItem value={"js"}>JavaScript</MenuItem>
+          <MenuItem value={"py"}>Python</MenuItem>
+        </Select>
+        </FormControl>
+        </div>
+
+
         <div className="textfields">
           <TextField
             className="filled-textarea"
