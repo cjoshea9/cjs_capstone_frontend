0c0c0f8bed571166429936bc1568c18ad574c1d9
Stephanie Walsh <srwalsh21@gmail.com>
2020-06-29 16:58:14 -0400

    Added google search bar and use rest API to query

--- src/App.js
+++ src/App.js
@@ -8,6 +8,7 @@ import InputLabel from '@material-ui/core/InputLabel';
 import MenuItem from '@material-ui/core/MenuItem';
 import FormControl from '@material-ui/core/FormControl';
 
+
 class App extends React.Component {
   constructor(props){
     super(props);
@@ -22,11 +23,17 @@ class App extends React.Component {
   }
   
   postRequest() {
+    // TODO: add error checking before post and get request
     const params = new URLSearchParams({
       input: this.state.input,
       in_lang: this.state.inputLanguage,
       out_lang: this.state.outputLangauge
     })
+
+    // sends get request with user query
+    const query = this.state.input + " in " + this.state.outputLangauge;
+    this.getRequest(query)
+
     fetch('https://cjsback.herokuapp.com/', {
       method: 'POST',
       headers: new Headers({
@@ -60,11 +67,41 @@ class App extends React.Component {
     });
   }
 
+  // Adds script for search bar
+  componentDidMount () {
+    const script = document.createElement("script");
+    script.src = "https://cse.google.com/cse.js?cx=013104617978576650762:hrpvx9uejrs";
+    script.async = true;
+    document.body.appendChild(script);
+  }
+
+  // Get request using rest API - returns all data (links, titles, summary etc)
+  getRequest(query) {
+    console.log(query);
+    fetch('https://www.googleapis.com/customsearch/v1?key=AIzaSyAU-3jbW41Be4Vj_1fI4kr6yf3hsOpCAbw&cx=013104617978576650762:hrpvx9uejrs&q=' + query, {
+      method: 'GET',
+      headers: new Headers({
+        'Content-Type': 'application/x-www-form-urlencoded',
+      })
+    }).then(res => res.json()).then((data) => {
+      for (const currElm of data.items) {
+        //html formatted has tags in it
+        const url = currElm.link;
+        const title = currElm.htmlTitle;
+        // div to hold individual result
+        const elm = document.createElement("a");
+        elm.classList.add("links");
+        elm.setAttribute("href", url);
+        elm.innerHTML = title;
+        document.body.appendChild(elm)
+      }
+    })
+  }
+  
   render() {
     return (
       <div>
         <Navbar/>
-        
        <div class="selectlanguage">
        <FormControl>
         <InputLabel>Input</InputLabel>
@@ -120,6 +157,10 @@ class App extends React.Component {
             Translate
           </Button>
         </div>
+        
+
+        <div class="gcse-search"></div>
+        
       </div>
     );
   }
