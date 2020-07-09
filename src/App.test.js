341be89aa64ac17d5cddaeb5fe5eccc8b48b2ce9
Jack Weber <jackdavidweber@gmail.com>
2020-07-09 15:30:09 -0700

    added tests

--- src/App.test.js
+++ src/App.test.js
@@ -2,8 +2,16 @@ import React from 'react';
 import { render } from '@testing-library/react';
 import App from './App';
 
-test('renders learn react link', () => {
+test('renders codeTranslate text', () => {
   const { getByText } = render(<App />);
-  const linkElement = getByText(/CodeTranslate/i);
-  expect(linkElement).toBeInTheDocument();
+  const appBarElement = getByText(/CodeTranslate/i);
+  expect(appBarElement).toBeInTheDocument();
+});
+
+test('renders input code label ', () => {
+  const { getByText } = render(<App />);
+  const codeLabel = getByText(/Input Code/i);
+
+  expect(codeLabel).toBeInTheDocument();
+  
 });
