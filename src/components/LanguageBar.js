9eeaad38538ee277ecfa47ff0e6f80124ef0627c
Jack Weber <jackdavidweber@gmail.com>
2020-07-17 15:49:02 -0700

    seems like backend population is working except for bash since bash only supports toCode

--- src/components/LanguageBar.js
+++ src/components/LanguageBar.js
@@ -5,34 +5,49 @@ import Tabs from '@material-ui/core/Tabs';
 import Tab from '@material-ui/core/Tab';
 
 
-export default function LanguageBar({inputLanguage, outputLanguage, handleInputLanguageChange, handleOutputLanguageChange, classes}) {
+export default function LanguageBar({supportedLanguages, inputLanguage, outputLanguage, handleInputLanguageChange, handleOutputLanguageChange, classes}) {
+    const langCodes = Object.keys(supportedLanguages);
+    
     return(
-        <AppBar position="static" className={classes.languageBar}>
+        <AppBar position="static" color="default">
             <Grid container className={classes.basicGrid} spacing={2}>
                 <Grid item xs={6}>
-                    <Tabs
+                    <Tabs 
+                      value={inputLanguage} 
+                      onChange={handleInputLanguageChange} 
+                      indicatorColor="primary" 
+                      textColor="primary" 
+                      variant="scrollable" 
+                      scrollButtons="auto"
+                    >
+                        {langCodes.map( code => (
+                            <Tab key={code} value={code} label={supportedLanguages[code]} />
+                        ))}
+                    </Tabs>
+                    {/* <Tabs
                         value={inputLanguage}
                         onChange={handleInputLanguageChange}
                         indicatorColor="primary"
                         textColor="primary"
                         variant="fullWidth"
-                    >
-                        <Tab value= "js" label="JavaScript" />
-                        <Tab value="py" label="Python" />
-                    </Tabs>
+                    > */}
+                    {/* </Tabs> */}
                 </Grid>
                 {/* TODO: break up both sets of tabs */}
                 <Grid item xs={6}>
-                    <Tabs
-                        value={outputLanguage}
-                        onChange={handleOutputLanguageChange}
-                        indicatorColor="primary"
-                        textColor="primary"
-                        variant="fullWidth"
+                    <Tabs 
+                      value={outputLanguage} 
+                      onChange={handleOutputLanguageChange} 
+                      indicatorColor="primary" 
+                      textColor="primary" 
+                      variant="scrollable" 
+                      scrollButtons="auto"
                     >
-                        <Tab value= "js" label="JavaScript" />
-                        <Tab value="py" label="Python" />
+                        {langCodes.map( code => (
+                            <Tab key={code} value={code} label={supportedLanguages[code]} />
+                        ))}
                     </Tabs>
+
                 </Grid>
             </Grid>
         </AppBar>
