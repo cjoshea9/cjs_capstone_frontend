e2961790b8c47e78a25c73397b0a42f5797e006e
Jack Weber <jackdavidweber@gmail.com>
2020-07-27 11:36:54 -0700

    cleaned up code and changed error color

--- src/components/TranslateBoxes.js
+++ src/components/TranslateBoxes.js
@@ -3,58 +3,37 @@ import Grid from '@material-ui/core/Grid';
 import Paper from '@material-ui/core/Paper';
 import TextField from '@material-ui/core/TextField';
 import Box from '@material-ui/core/Box';
-import { Typography, Tooltip } from '@material-ui/core';
+import { Typography, Tooltip, Link } from '@material-ui/core';
 
 
 export default function TranslateBoxes({input, handleInputChange, output, errors, classes}){
-    function addErrorsToOutput(output, errors){
-        console.log(output)
-        if (errors && Object.entries(errors).length !== 0){
-            let finalOutput = output
-
-            // go through output text and find any strings with pattern $$E_$$
-            const regexp = /\$\$E.\$\$/g
-            const matches = [...output.matchAll(regexp)];
-
-            for (const match of matches){
-                const errorKey = match[0].substring(2).slice(0,-2)
-                const errorMessage = errors[errorKey]["errorMessage"]
-                finalOutput = finalOutput.replace(match, errorMessage)
-            }
-            return finalOutput
-
-        } else {
-            return output
-        }
-    }
 
     function createOutputTypography(output, errors){
         const regexp = /\$\$E.\$\$/g
         const matches = [...output.matchAll(regexp)];
-        if (matches.length == 0){
-            return <Typography style={{whiteSpace: 'pre-line'}} display="inline">{output}</Typography>
-        }
 
-        if (errors && Object.entries(errors).length !== 0){
+        if (matches.length > 0 && errors && Object.entries(errors).length !== 0){
+            console.log(matches)
             let prev = 0;
             let finalOutput = []
             for (const match of matches){
-                // non error
+                // Add text before error
                 let beforeText = output.substring(prev, match["index"])
-                finalOutput.push(<Typography style={{whiteSpace: 'pre-line'}} key={prev} display="inline">{beforeText}</Typography>)
+                finalOutput.push(<Typography className={classes.outText} key={prev}>{beforeText}</Typography>)
 
-                // error
+                // Add error text
                 let errorKey = match[0].substring(2).slice(0,-2)
                 let errorMessage = errors[errorKey]["errorMessage"]
-                finalOutput.push(<Tooltip style={{whiteSpace: 'pre-line'}} key={prev+1} title={errorMessage}><Typography display="inline">Error</Typography></Tooltip>)
-                console.log("prev1", prev)
+                finalOutput.push(<Tooltip key={prev+1} title={errorMessage} arrow><Typography className={classes.outErrorText}><Link color="inherit">{"<"}err{">"}</Link></Typography></Tooltip>)
                 prev = match["index"] + (errorKey.length) + 4;
-                console.log("prev2", prev)
             }
             let afterText = output.substring(prev)
-            finalOutput.push(<Typography style={{whiteSpace: 'pre-line'}} key={prev} display="inline">{afterText}</Typography>)
+            finalOutput.push(<Typography className={classes.outText} key={prev}>{afterText}</Typography>)
             return finalOutput
         }
+
+        // if no errors, just return output as string
+        return [<Typography key={0} className={classes.outText}>{output}</Typography>]
     }
 
     return(
@@ -78,11 +57,7 @@ export default function TranslateBoxes({input, handleInputChange, output, errors
                 </Grid>
                 <Grid item xs={6}>
                     <Box className = {classes.box} bgcolor="grey.200">
-                        {/* <Typography >
-                          {errors && output && addErrorsToOutput(output, errors)}
-                        </Typography> */}
                         {errors && output && createOutputTypography(output, errors)}
-                        {/* <Tooltip title="Hi"><Typography display="inline">Hello</Typography></Tooltip> */}
                     </Box>
                 </Grid>
             </Grid>
